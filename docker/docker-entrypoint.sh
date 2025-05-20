873c5cf0a8033fee580afd99ac959f1656054104
pentium10 <hexagonhu@yahoo.com>
2025-05-21 00:52:56 +0300

    Improve Beanstalkd host in Dockerfile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 set -e
 
-
 # Change ownership for apache happiness
 chown -R www-data:www-data "${APACHE_DOCROOT}"
 
-if [[ -n "$BEANSTALKD_HOST" ]]; then
+TARGET_LINE_PATTERN="^\s*'servers'\s*=>\s*array.*"
 
+if [[ -n "$BEANSTALKD_HOST" ]]; then
   if [[ -z "$BEANSTALKD_PORT" ]]; then
     BEANSTALKD_PORT=11300
   fi
-
-  sed -ir "s/'servers'.*$/'servers'=> array('Default Beanstalkd' => 'beanstalk:\/\/$BEANSTALKD_HOST:$BEANSTALKD_PORT'),/g" /var/www/config.php
+  REPLACEMENT_STRING="    'servers' => array('$BEANSTALKD_HOST' => 'beanstalk://$BEANSTALKD_HOST:$BEANSTALKD_PORT'),"
+  sed -i "s#${TARGET_LINE_PATTERN}#${REPLACEMENT_STRING}#g" /var/www/config.php
 
 elif [[ -n "$BEANSTALKD_PORT_11300_TCP_ADDR" ]]; then
-
   BEANSTALKD_HOST=$BEANSTALKD_PORT_11300_TCP_ADDR
-
   if [[ -z "$BEANSTALKD_PORT" ]]; then
     if [[ -n "$BEANSTALKD_PORT_11300_TCP_PORT" ]]; then
       BEANSTALKD_PORT=$BEANSTALKD_PORT_11300_TCP_PORT
+    else # Default if specific port not found via link
+      BEANSTALKD_PORT=11300
     fi
-  else
-    BEANSTALKD_PORT=11300
+  fi
+  if [[ -z "$BEANSTALKD_PORT" ]]; then # If BEANSTALKD_PORT (global) was not set
+    if [[ -n "$BEANSTALKD_PORT_11300_TCP_PORT" ]]; then
+      BEANSTALKD_PORT=$BEANSTALKD_PORT_11300_TCP_PORT
+    else
+      BEANSTALKD_PORT=11300 # Fallback if linked port is also not set
+    fi
   fi
 
-  sed -ir "s/'servers'.*$/'servers'=> array('Default Beanstalkd' => 'beanstalk:\/\/$BEANSTALKD_HOST:$BEANSTALKD_PORT'),/g" /var/www/config.php
-
+  REPLACEMENT_STRING="    'servers' => array('$BEANSTALKD_HOST' => 'beanstalk://$BEANSTALKD_HOST:$BEANSTALKD_PORT'),"
+  sed -i "s#${TARGET_LINE_PATTERN}#${REPLACEMENT_STRING}#g" /var/www/config.php
 fi
 
 rm -f /var/run/apache2/apache2.pid
